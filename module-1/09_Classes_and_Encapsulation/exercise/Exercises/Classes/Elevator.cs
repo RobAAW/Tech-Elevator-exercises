4b000f52120280191e648cbbf626216144adbac7
Aaron Ward <r.aaronward@gmail.com>
2022-01-28 16:20:22 -0500

    Encapsulation 99%

--- module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/Elevator.cs
+++ module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/Elevator.cs
@@ -3,6 +3,63 @@
     public class Elevator
     {
 
+        public int CurrentLevel { get; private set; }
+        public int NumberOfLevels { get; private set; }
+        public bool DoorIsOpen { get; private set; }
+
+        public Elevator (int numberOfLevels)
+        {
+            NumberOfLevels = numberOfLevels;
+            DoorIsOpen = false;
+            CurrentLevel = 1;
+        }
+
+        
+
+        public void OpenDoor()
+        {
+            if (!DoorIsOpen)
+            {
+                DoorIsOpen = true;
+            }
+        }
+
+        public void CloseDoor()
+        {
+            if (DoorIsOpen)
+            {
+                DoorIsOpen = false;
+            }
+        }
+
+        public void GoUp(int desiredFloor)
+        {
+            if (DoorIsOpen)
+            {
+                DoorIsOpen = false;
+            }
+            else if (desiredFloor <= NumberOfLevels && desiredFloor > CurrentLevel)
+            {
+                CurrentLevel = desiredFloor;
+            }
+        }
+
+        public void GoDown(int desiredFloor)
+        {
+            if (DoorIsOpen)
+            {
+                DoorIsOpen = false;
+            }
+            else if (desiredFloor >= NumberOfLevels && desiredFloor < CurrentLevel)
+            {
+                CurrentLevel = desiredFloor;
+            }
+        }
+
+
+        
+        
+
 
     }
 }
