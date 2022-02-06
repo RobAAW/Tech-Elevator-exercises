68416ef22fe7ab595be23e1cdc258b44b07551b7
Aaron Ward <r.aaronward@gmail.com>
2022-02-05 22:46:10 -0500

    C:/Program Files/git/14_Unit_Testing/ complete

---
+++ module-1/14_Unit_Testing/exercise/Exercises.Tests/SameFirstLastTest.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class SameFirstLastTest
+    {
+        //array of ints
+        //return true if the array is length 1 or more
+        //and the first element and the last element are equal.
+        //bool IsItTheSame(int[] nums)
+
+        [TestMethod]
+        public void LengthIsLessThanOne()
+        {
+            //arrange
+            SameFirstLast sut = new SameFirstLast();
+
+            //act
+            int[] nums = new int[] {};
+            bool hasNone = sut.IsItTheSame(nums);
+
+
+            //assert
+            Assert.IsFalse(hasNone);
+        }
+
+        [TestMethod]
+        public void FirstLastEqual()
+        {
+            //arrange
+            SameFirstLast sut = new SameFirstLast();
+
+            //act
+            int[] nums = new int[] {1, 2, 1 };
+            bool firstLast = sut.IsItTheSame(nums);
+
+
+            //assert
+            Assert.IsTrue(firstLast);
+        }
+
+        [TestMethod]
+        public void KindaButNegative()
+        {
+            //arrange
+            SameFirstLast sut = new SameFirstLast();
+
+            //act
+            int[] nums = new int[] {-1, 2, 1 };
+            bool aNegative = sut.IsItTheSame(nums);
+
+
+            //assert
+            Assert.IsFalse(aNegative);
+        }
+
+        [TestMethod]
+        public void EdgeCase()
+        {
+            //arrange
+            SameFirstLast sut = new SameFirstLast();
+
+            //act
+            int[] nums = new int[] { 2147483647, 1, 2147483647 };
+            bool edgeCase = sut.IsItTheSame(nums);
+
+
+            //assert
+            Assert.IsTrue(edgeCase);
+        }
+
+        [TestMethod]
+        public void EdgeCaseTwo()
+        {
+            //arrange
+            SameFirstLast sut = new SameFirstLast();
+
+            //act
+            int[] nums = new int[] { -2147483647, 1, -2147483647 };
+            bool edgeCaseTwo = sut.IsItTheSame(nums);
+
+
+            //assert
+            Assert.IsTrue(edgeCaseTwo);
+        }
+
+        [TestMethod]
+        public void EqualZeroes()
+        {
+            //arrange
+            SameFirstLast sut = new SameFirstLast();
+
+            //act
+            int[] nums = new int[] { 0, 2147483647, 0 };
+            bool bothZeroes = sut.IsItTheSame(nums);
+
+
+            //assert
+            Assert.IsTrue(bothZeroes);
+        }
+
+
+    }
+}
--- module-1/14_Unit_Testing/exercise/Exercises.Tests/WordCountTest.cs
+++ module-1/14_Unit_Testing/exercise/Exercises.Tests/WordCountTest.cs
@@ -1,10 +1,81 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Exercises.Tests
 {
-    class WordCountTest
+    [TestClass]
+    public class WordCountTest
     {
+        //Given an array of strings, return a Dictionary<string, int> with a key for each different
+        //string, with the value the number of times that string appears in the array.
+        //Dictionary<string, int> GetCount(string[] words)
+
+
+        [TestMethod]
+        public void AppearsOnce()
+        {
+            //arrange
+            WordCount sut = new WordCount();
+
+            //act
+            Dictionary<string, int> output = new Dictionary<string, int>() { { "Super", 1 }, { "Mario", 1 }, { "Bros", 1 } };
+            string[] words = new string[] { "Super", "Mario", "Bros" };
+
+            Dictionary<string, int> compare = sut.GetCount(words);
+
+            //assert
+            CollectionAssert.AreEqual(output, compare);
+        }
+
+        [TestMethod]
+        public void AppearsMoreThanOnce()
+        {
+            //arrange
+            WordCount sut = new WordCount();
+
+            //act
+            Dictionary<string, int> output = new Dictionary<string, int>() { { "Dance", 2 }, { "Revolution", 1 } };
+            string[] words = new string[] { "Dance", "Dance", "Revolution" };
+
+            Dictionary<string, int> compare = sut.GetCount(words);
+
+            //assert
+            CollectionAssert.AreEqual(output, compare);
+        }
+
+        [TestMethod]
+        public void StringIsEmpty()
+        {
+            //arrange
+            WordCount sut = new WordCount();
+
+            //act
+            Dictionary<string, int> output = new Dictionary<string, int>() { { "", 2 }, { "Hades", 1 } };
+            string[] words = new string[] { "", "Hades", "" };
+
+            Dictionary<string, int> compare = sut.GetCount(words);
+
+            //assert
+            CollectionAssert.AreEqual(output, compare);
+        }
+
+        [TestMethod]
+        public void NumberStringsDoNotMatch()
+        {
+            //arrange
+            WordCount sut = new WordCount();
+
+            //act
+            Dictionary<string, int> output = new Dictionary<string, int>() { { "2147483647", 2 }, { "Metroid", 1 } };
+            string[] words = new string[] { "2147483647", "Hades", "2137483647" };
+
+            Dictionary<string, int> compare = sut.GetCount(words);
+
+            //assert
+            CollectionAssert.AreNotEqual(output, compare);
+        }
+
     }
 }
