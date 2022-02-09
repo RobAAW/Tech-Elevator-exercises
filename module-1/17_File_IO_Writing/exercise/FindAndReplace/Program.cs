eed58525c78abfbd9e90098019a165ad9129316f
Aaron Ward <r.aaronward@gmail.com>
2022-02-09 13:48:28 -0500

    IO Writing complete

--- module-1/17_File_IO_Writing/exercise/FindAndReplace/Program.cs
+++ module-1/17_File_IO_Writing/exercise/FindAndReplace/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FindAndReplace
 {
@@ -6,7 +7,53 @@ namespace FindAndReplace
     {
 		public static void Main(string[] args)
 		{
-            Console.WriteLine("Hello World!");
+            // Prompt the user for a search word
+            Console.WriteLine("What is the search word? ");
+            string searchWord = Console.ReadLine();
+            //searchWord = searchWord.ToLower();
+
+            // Prompt the user for a replacement word
+            Console.WriteLine("What is the replacement word? ");
+            string replaceWord = Console.ReadLine();
+
+            // What is the source file?
+            Console.WriteLine("Please enter the path to the source file: ");
+            string sourceFile = Console.ReadLine();
+
+            // What is the destination file?
+            Console.WriteLine("Please enter the desired destination file: ");
+            string destinationFile = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(sourceFile))
+                using (StreamWriter sw = new StreamWriter(destinationFile))
+                {
+                    // Loop until the end of file is reached
+                    while (!sr.EndOfStream)
+                    {
+                        // Read an individual line
+                        string line = sr.ReadLine();
+
+                        // Replace the occurence of the word langauge with language
+                        string fixedLine = line.Replace(searchWord, replaceWord);
+
+                        // Write the new line to the output file
+                        sw.WriteLine(fixedLine);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("*** Problem reading specified file: " + ex.Message);
+                return;
+            }
+
+            // Tell the user what happened.
+            /*string message = $"Converted {searchWord} to {replaceWord} " +
+                             $"to {destinationFile} on {DateTime.Now}";
+            Console.WriteLine(message);*/
+
         }
     }
 }
