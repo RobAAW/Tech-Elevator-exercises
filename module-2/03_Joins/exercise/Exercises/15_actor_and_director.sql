24f256e33591191c345f77d677ac9e27e39dad88
Aaron Ward <r.aaronward@gmail.com>
2022-02-17 18:53:03 -0500

    module-2/03_Joins/exercise/

--- module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
+++ module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
@@ -1,2 +1,10 @@
 -- 15. The title of the movie and the name of director for movies where the director was also an actor in the same movie (73 rows)
 
+SELECT m.title, p.person_name
+FROM movie m
+JOIN person p
+ON m.director_id = p.person_id
+JOIN movie_actor ma
+ON m.movie_id = ma.movie_id
+WHERE m.director_id = ma.actor_id;
+
