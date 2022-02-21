eedc207d41e10c2650bad54ccc5b672e3d1c9308
Aaron Ward <r.aaronward@gmail.com>
2022-02-20 19:07:25 -0500

    INSERT_UPDATE_and_DELETE/exercise

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/07_remove_actors.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/07_remove_actors.sql
@@ -1,3 +1,18 @@
 -- 7. Remove the actor appearances in "Avengers: Infinity War" (14 rows)
 -- Note: Don't remove the actors themeselves, just make it so it seems no one appeared in the movie.
 
+SELECT *
+FROM movie_actor
+WHERE (movie_id = (SELECT movie_id FROM movie WHERE title = 'Avengers: Infinity War'));
+
+SELECT *
+FROM movie_actor;
+
+BEGIN TRANSACTION;
+DELETE FROM movie_actor
+WHERE actor_id IN (SELECT movie_id FROM movie WHERE title = 'Avengers: Infinity War');
+ROLLBACK;
+
+
+
+SELECT title FROM movie WHERE title = 'Avengers: Infinity War'
