9d8e70774e9e8c7510e99d4ece74fe321bb4c1fb
Tom Anderson <tom.anderson@techelevator.com>
2022-02-25 12:50:23 -0500

    New material

---
+++ vvm/VendingMachine/DAO/DBDAO.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using VendingMachine.Classes;
+
+namespace VendingMachine.DAO
+{
+    public class DBDAO : IDAO
+    {
+        private readonly string connectionString;
+        public DBDAO(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public Dictionary<string, Queue<VendingMachineItem>> LoadMachine(string filePath)
+        {
+            // let's get the inventory from the database
+            Dictionary<string, Queue<VendingMachineItem>> inventory = new Dictionary<string, Queue<VendingMachineItem>>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand sql = new SqlCommand("select * from VM_Items where isActive=1 order by SlotID;", conn);
+                    SqlDataReader sqlDataReader = sql.ExecuteReader();
+                    while (sqlDataReader.Read())
+                    {
+                        string slot = Convert.ToString(sqlDataReader["SlotID"]);
+                        inventory[slot] = CreateVMItemQueueFromReader(sqlDataReader);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return inventory;
+        }
+
+        private Queue<VendingMachineItem> CreateVMItemQueueFromReader(SqlDataReader reader)
+        {
+            Queue<VendingMachineItem> queue = new Queue<VendingMachineItem>();
+            string itemDesc = Convert.ToString(reader["Description"]);
+            decimal price = Convert.ToDecimal(reader["Price"]);
+            string snackType = Convert.ToString(reader["SnackType"]);
+
+            switch (Convert.ToString(reader["SnackType"]))
+            {
+                case "Chip":
+                    //instantiate the Queue
+                    queue = new Queue<VendingMachineItem>();
+                    // Add 5 objects to the queue
+                    for (int i = 0; i < 5; i++)
+                    {
+                        // declare an instantiate the product item
+                        ChipItem chipItem = new ChipItem(itemDesc, price);
+                        // Add it to the queue
+                        queue.Enqueue(chipItem);
+                    }
+                    break;
+                case "Drink":
+                    //instantiate the Queue
+                    queue = new Queue<VendingMachineItem>();
+                    // Add 5 objects to the queue
+                    for (int i = 0; i < 5; i++)
+                    {
+                        // declare an instantiate the product item
+                        BeverageItem chipItem = new BeverageItem(itemDesc, price);
+                        // Add it to the queue
+                        queue.Enqueue(chipItem);
+                    }
+                    break;
+                case "Candy":
+                    queue = new Queue<VendingMachineItem>();
+                    for (int i = 0; i < 5; i++)
+                    {
+                        CandyItem chipItem = new CandyItem(itemDesc, price);
+                        queue.Enqueue(chipItem);
+                    }
+                    break;
+                case "Gum":
+                    queue = new Queue<VendingMachineItem>();
+                    for (int i = 0; i < 5; i++)
+                    {
+                        GumItem chipItem = new GumItem(itemDesc, price);
+                        queue.Enqueue(chipItem);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return queue;
+        }
+
+        public void LogTransaction(string action, decimal txAmount, decimal currentBalance)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand sqlCommand = new SqlCommand("insert into VM_Log(Action,StartValue,EndValue) " +
+                        "Values(@action,@startValue,@endValue)", conn);
+                    sqlCommand.Parameters.AddWithValue("@action", action);
+                    sqlCommand.Parameters.AddWithValue("@startValue", txAmount);
+                    sqlCommand.Parameters.AddWithValue("@endValue", currentBalance);
+                    sqlCommand.ExecuteNonQuery();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void ResetLog()
+        {
+            // Don't do anything, keep stuff in the db
+        }
+    }
+}
---
+++ vvm/VendingMachine/DAO/FileDAO.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using VendingMachine.Classes;
+
+namespace VendingMachine.DAO
+{
+    public class FileDAO : IDAO
+    {
+        public Dictionary<string, Queue<VendingMachineItem>> LoadMachine(string filePath)
+        {
+            // Declare a local variable to hold the generated dictionary
+            Dictionary<string, Queue<VendingMachineItem>> result = new Dictionary<string, Queue<VendingMachineItem>>();
+
+            // Does the file exist?
+            if (File.Exists(filePath))
+            {
+                // read in the file and populate the dictionary
+                try
+                {
+                    using (StreamReader sr = new StreamReader(filePath))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            // Read the line from the inventory file
+                            string line = sr.ReadLine();
+                            // The items are separated by the | character, split each value into an item in an array
+                            string[] item = line.Split('|');
+
+                            // Set an "out" variable to use in TryParse
+                            decimal price = 0;
+                            // Try parse kind of combines a try/catch block and a parse. If the parse succeeds, it returns true.
+                            decimal.TryParse(item[2], out price);
+                            // Set up a local queue that represents the items in a slot
+                            Queue<VendingMachineItem> queue = new Queue<VendingMachineItem>();
+                            // Check the type of item in a slot based on the input files
+                            switch (item[3])
+                            {
+                                case "Chip":
+                                    //instantiate the Queue
+                                    queue = new Queue<VendingMachineItem>();
+                                    // Add 5 objects to the queue
+                                    for (int i = 0; i < 5; i++)
+                                    {
+                                        // declare an instantiate the product item
+                                        ChipItem chipItem = new ChipItem(item[1], price);
+                                        // Add it to the queue
+                                        queue.Enqueue(chipItem);
+                                    }
+                                    result[item[0]] = queue;
+                                    break;
+                                case "Drink":
+                                    //instantiate the Queue
+                                    queue = new Queue<VendingMachineItem>();
+                                    // Add 5 objects to the queue
+                                    for (int i = 0; i < 5; i++)
+                                    {
+                                        // declare an instantiate the product item
+                                        BeverageItem chipItem = new BeverageItem(item[1], price);
+                                        // Add it to the queue
+                                        queue.Enqueue(chipItem);
+                                    }
+                                    result[item[0]] = queue;
+                                    break;
+                                case "Candy":
+                                    queue = new Queue<VendingMachineItem>();
+                                    for (int i = 0; i < 5; i++)
+                                    {
+                                        CandyItem chipItem = new CandyItem(item[1], price);
+                                        queue.Enqueue(chipItem);
+                                    }
+                                    result[item[0]] = queue;
+                                    break;
+                                case "Gum":
+                                    queue = new Queue<VendingMachineItem>();
+                                    for (int i = 0; i < 5; i++)
+                                    {
+                                        GumItem chipItem = new GumItem(item[1], price);
+                                        queue.Enqueue(chipItem);
+                                    }
+                                    result[item[0]] = queue;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+            return result;
+        }
+
+        public void LogTransaction(string action, decimal txAmount, decimal currentBalance)
+        {
+            // Get the current directory
+            string directory = Environment.CurrentDirectory;
+            // Put together the fully qualified path to the log file
+            string fullPath = Path.Combine(directory, "Log.txt");
+            // Try block since we're doing something dangerous: reading a file
+            try
+            {
+                // open a streamwriter to allow us to write to the file. Use the true parameter to append
+                using (StreamWriter sw = new StreamWriter(fullPath, true))
+                {
+                    // write the line to the log
+                    sw.WriteLine($"{DateTime.UtcNow.ToString()} {action}: {txAmount:C2} {currentBalance:C2}");
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public void ResetLog()
+        {
+            // Get the current directory
+            string directory = Environment.CurrentDirectory;
+            // Put together the fully qualified path to the log file
+            string fullPath = Path.Combine(directory, "Log.txt");
+            // Try block since we're doing something dangerous: accessing a file
+            try
+            {
+                // Delete the file
+                File.Delete(fullPath);
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
