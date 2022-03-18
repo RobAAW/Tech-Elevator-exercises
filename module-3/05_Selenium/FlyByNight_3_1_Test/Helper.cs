884cb82f458d94d20754eba1f1472cb0e27a724f
Tom Anderson <tom.anderson@techelevator.com>
2022-03-18 08:59:23 -0400

    New material

---
+++ module-3/05_Selenium/FlyByNight_3_1_Test/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlyByNight_3_1_Test
+{
+    public static class Helper
+    {
+        // TODO Change this value to whatever port your VS is using.
+        // Also, load your project in another window before running tests and launch it. 
+        // Looke at the url, and update the port below.
+        // The web server must be running for the tests to work.
+        public static readonly string URL = "https://localhost:44381/";
+    }
+}
---
+++ module-3/05_Selenium/FlyByNight_3_1_Test/MortgageCalculatorIntegrationTestWithPageObject.cs
@@ -0,0 +1,59 @@
+﻿using FlyByNight_3_1_Test.PageObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace FlyByNight_3_1_Test
+{
+    [TestClass]
+    public class MortgageCalculatorIntegrationTestWithPageObject
+    {
+        private static IWebDriver webDriver;
+        private HomePage homePage;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            webDriver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            webDriver.Navigate().GoToUrl(Helper.URL);
+            homePage = new HomePage(webDriver);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            webDriver.Quit();
+            //webDriver.Close();
+        }
+
+        [TestMethod]
+        public void forms_can_be_edited_and_submitted()
+        {
+            MortgageCalculatorResultPage resultPage = homePage.clickMortgageCalculatorLink()
+                                                        .enterLoanAmount("100000")
+                                                        .enterLoanTerm("15 Years")
+                                                        .enterInterestRate("4.5")
+                                                        .submitForm();
+
+            Assert.AreEqual("$764.99", resultPage.getPaymentAmount());
+        }
+
+        [TestMethod]
+        public void forms_can_be_edited_and_submitted_high_value()
+        {
+            MortgageCalculatorResultPage resultPage = homePage.clickMortgageCalculatorLink()
+                                                        .enterLoanAmount("1000000")
+                                                        .enterLoanTerm("30 Years")
+                                                        .enterInterestRate("5.5")
+                                                        .submitForm();
+
+            Assert.AreEqual("$5,677.89", resultPage.getPaymentAmount());
+        }
+
+    }
+}
